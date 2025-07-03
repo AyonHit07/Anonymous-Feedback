677d3170a709514cda5ddaf15916210c60a65ab8
AyonHit07 <ayonhit07@gmail.com>
2025-07-03 18:23:02 +0530

    Updated middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
   if (token &&
     (
       url.pathname.startsWith('/sign-in') ||
-      url.pathname.startsWith('/siign-up') ||
+      url.pathname.startsWith('/sign-up') ||
       url.pathname.startsWith('/verify') ||
       url.pathname.startsWith('/')
     )
